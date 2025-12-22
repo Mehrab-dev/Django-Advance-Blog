9d5e054ab1c002d270afe67b6e86326bb46eb6cd
Mehrab <programmer.py.mail@gmail.com>
2025-12-22 22:51:40 +0330

    creating CustomUserAdmin class in account.admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from account.models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin) :
+    model = User
+    list_display = ['email','is_superuser','is_staff','is_active']
+    list_filter = ['is_superuser','is_active']
+    search_fields = ['email']
+    ordering = ('email',)
+
+admin.site.register(User,CustomUserAdmin)
--- account/models.py
+++ account/models.py
@@ -28,7 +28,7 @@ class User(AbstractBaseUser,PermissionsMixin) :
     email = models.EmailField(max_length=255,unique=True)
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    ia_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
