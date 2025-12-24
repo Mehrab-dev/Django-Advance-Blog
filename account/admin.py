2b5f11152304476c8d1b781066ce0ad3becdd1a4
Mehrab <programmer.py.mail@gmail.com>
2025-12-24 15:59:47 +0330

    completing admin in account app
    set up the fieldsets (Authentication , Permission , Group-Permission , important-data) and add_fieldsets in CustomUserAdmin in account.admin
    
    creating save_profile function with receiver decorator

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from account.models import User
+from account.models import User , Profile
 
 
 class CustomUserAdmin(UserAdmin) :
@@ -10,4 +10,29 @@ class CustomUserAdmin(UserAdmin) :
     search_fields = ['email']
     ordering = ('email',)
 
+    fieldsets = (
+        ('Authentication',{
+            'fields':('email','password')
+        }),
+        ('Permission',{
+            'fields':('is_superuser','is_staff','is_active')
+        }),
+        ('Group Permission',{
+            'fields':('groups','user_permissions')
+        }),
+        ('important date',{
+            'fields':('last_login',)
+        }),
+    )
+
+    add_fieldsets = (
+        (None,{
+            'classes':('wide'),
+            'fields':('email','password1','password2','is_superuser','is_staff','is_active'),
+        }),
+    )
+
+
+
+admin.site.register(Profile)
 admin.site.register(User,CustomUserAdmin)
