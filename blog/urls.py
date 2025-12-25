f7ba856173ef9a70fce1b0496c9ca5799554fdd7
Mehrab <programmer.py.mail@gmail.com>
2025-12-25 13:23:10 +0330

    ClassBaseViews
    creating RedirectViews (cbv) in blog.views
    creating PostListView , PostDetail ,  CreatePost , UpdatePost , DeletePost with genenric
    creating forms.py in blog app and PostForm class in it

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,11 @@ from django.urls import path
 app_name = 'blog'
 
 urlpatterns = [
-    path('',views.IndexView.as_view(),name='index_view')
+    path('',views.IndexView.as_view(),name='index_view'),
+    path('redirect/',views.RedirectViews.as_view(),name='redirect_view'),
+    path('posts/',views.PostListView.as_view(),name='post_list'),
+    path('post/<int:pk>/',views.PostDetail.as_view(),name='post_detail'),
+    path('create/post/',views.CreatePost.as_view(),name='create_post'),
+    path('update/<int:pk>/',views.UpdatePost.as_view(),name='update_post'),
+    path('delete/<int:pk>/',views.DeletePost.as_view(),name='delete_post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView , RedirectView
+from django.views.generic import ListView , DetailView , CreateView , UpdateView , DeleteView
 from .models import Post
+from .forms import PostForm
 
 class IndexView(TemplateView) :
     template_name = 'blog/index.html'
@@ -9,3 +11,38 @@ class IndexView(TemplateView) :
         context['name'] = 'mehrab'
         context['post'] = Post.objects.all()
         return context
+    
+class RedirectViews(RedirectView) :
+    url = "https://www.django-rest-framework.org/"
+
+class PostListView(ListView) :
+    # model = Post
+    # queryset = Post.objects.filter(status=True)
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        posts = Post.objects.filter(status=True)
+        return posts
+    
+class PostDetail(DetailView) :
+    model = Post
+    context_object_name = 'post'
+
+class CreatePost(CreateView) :
+    model = Post
+    form_class = PostForm
+    success_url = '/blog/posts/'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user.profile
+        return super().form_valid(form)
+
+class UpdatePost(UpdateView) :
+    model = Post
+    form_class = PostForm
+    success_url = '/blog/posts/'
+
+class DeletePost(DeleteView) :
+    model = Post
+    success_url = '/blog/posts/'
+    context_object_name = 'post'
